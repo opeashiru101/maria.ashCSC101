2e1dbbb5e7747dc3b761c5199ec73eda732c8059
opeashiru101 <opeash234@gmaiil.com>
2022-11-18 22:20:09 +0100

    (Week 6)

---
+++ week-6/practice_5/target/debug/practice_5.d
@@ -0,0 +1,1 @@
+C:\Users\opeas\Documents\maria.ashCSC101\week-6\practice_5\target\debug\practice_5.exe: C:\Users\opeas\Documents\maria.ashCSC101\week-6\practice_5\src\main.rs
---
+++ week-6/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\opeas\Documents\maria.ashCSC101\week-6\practice_6\target\debug\practice_6.exe: C:\Users\opeas\Documents\maria.ashCSC101\week-6\practice_6\src\main.rs
